1dc6fec443fdf336546ba698bdf6a207e862e5de
Oliver Schulz <oschulz@mpp.mpg.de>
2021-04-30 23:37:53 +0200

    STASH forwarddiff utils

--- test/differentiation/test_forwarddiff_utils.jl
+++ test/differentiation/test_forwarddiff_utils.jl
@@ -4,7 +4,7 @@ using BAT
 using Test
 
 using LinearAlgebra
-using StaticArrays, ForwardDiff
+using StaticArrays, ForwardDiff, Zygote
 
 @testset "forwarddiff_utils" begin
     f = (x...) -> (sum(map(x -> norm(x)^2, x)), sum(map(x -> norm(x)^3, x)), 42)
@@ -13,11 +13,11 @@ using StaticArrays, ForwardDiff
     DTf32 = ForwardDiff.Dual{ForwardDiff.Tag{typeof(f),Float32}}
     @test @inferred(BAT.forwarddiff_eval(f, 4f0)) == (DTf32(16.0,8.0), DTf32(64.0,48.0), 42)
     rdf = (DTf32(26.0,6.0,2.0,8.0), DTf32(92.0,27.0,3.0,48.0), 42)
-    @test @inferred(BAT.forwarddiff_eval(f, 3.0, true, 4f0)) == rdf
-    @test @inferred(BAT.forwarddiff_eval(fv, 3.0, true, 4f0)) == SVector(rdf)
+    @test @inferred(BAT.forwarddiff_eval(f, 3, true, 4f0)) == rdf
+    @test @inferred(BAT.forwarddiff_eval(fv, 3, true, 4f0)) == SVector(rdf)
 
-    x = SVector(3.0, true, 4f0)
-    r_ref = (ntuple(i -> DTf32(f(x)[i], ForwardDiff.jacobian(x -> SVector(f(x)[i]), SVector(3.0, true, 4f0))...), Val(2))..., 42)
+    x = SVector(3, true, 4f0)
+    r_ref = (ntuple(i -> DTf32(f(x)[i], ForwardDiff.jacobian(x -> SVector(f(x)[i]), SVector(3, true, 4f0))...), Val(2))..., 42)
     @test @inferred(BAT.forwarddiff_eval(f, (x...,))) == r_ref
     @test @inferred(BAT.forwarddiff_eval(f, x)) == r_ref
 
@@ -27,9 +27,15 @@ using StaticArrays, ForwardDiff
 
     @test @inferred(BAT.forwarddiff_vjp(0.7, BAT.forwarddiff_eval(sin, 0.5))) == (0.7 * ForwardDiff.derivative(sin, 0.5),)
 
+    x = SVector(3, true, 4f0)
     ΔΩ = SVector(10, 20, 30)
     @test @inferred(BAT.forwarddiff_vjp((ΔΩ...,), BAT.forwarddiff_eval(f, x))) == (ForwardDiff.jacobian(x -> SVector(f(x)), x)' * ΔΩ...,)
     @test @inferred(BAT.forwarddiff_vjp(ΔΩ, BAT.forwarddiff_eval(fv, x))) == (ForwardDiff.jacobian(fv, x)' * ΔΩ...,)
+
+    x = (3, true, 4f0, 7)
+    ΔΩ = (10, 20, 30)
+    @test all(map(isapprox, Base.tail(@inferred ChainRulesCore.rrule(BAT.WithForwardDiff(f), x)[2](ΔΩ))[1], Zygote.pullback(f, x)[2](ΔΩ)[1]))
+    @test all(map(isapprox, Base.tail(@inferred ChainRulesCore.rrule(BAT.WithForwardDiff(f), x...)[2](ΔΩ)), Zygote.pullback(f, x...)[2](ΔΩ)))
 
     #@test @inferred((x -> BAT.forwarddiff_pullback(f, x)[1])(x)) == f(x)
     #@test @inferred(BAT.forwarddiff_pullback(f, x)[2](ΔΩ)) == ForwardDiff.jacobian(f, x)' * ΔΩ
