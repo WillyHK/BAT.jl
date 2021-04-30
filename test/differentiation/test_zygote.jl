7d0486f2157035006fdc40c5bb73ec43915c43cf
Oliver Schulz <oschulz@mpp.mpg.de>
2021-04-30 14:20:49 +0200

    STASH Zygote tests

--- test/differentiation/test_zygote.jl
+++ test/differentiation/test_zygote.jl
@@ -8,15 +8,7 @@ using LinearAlgebra
 using ForwardDiff, Zygote
 
 @testset "zygote" begin
-    prior = NamedTupleDist(
-        a = Exponential(),
-        b = [4.2, 3.3],
-        c = Normal(1, 3),
-        d = [Weibull(), Weibull()],
-        e = Beta(),
-        f = MvNormal([0.3, -2.9], [1.7 0.5; 0.5 2.3])
-    )
-
+    prior = BAT.example_posterior().prior.dist
     likelihood = (;logdensity = v -> sqrt(sum(map(x -> norm(x)^2, values(v)))))
 
     # Causes Zygote to fail with "Can't differentiate foreigncall expression" exception:
@@ -63,5 +55,13 @@ fwddiff_trafo_src_v(dst_v, src_v, 0.5, 0)
 
 
 # Scratch: ==================================================
+
+using BAT, Distributions
+
+trafo = BAT.DistributionTransform(Normal(), Weibull())
+v = 0.5
+@test @inferred(trafo(v)) == quantile(trafo.target_dist, cdf(trafo.source_dist, v))
+
+BAT.forwarddiff_pullback(trafo, v)
 
 =#
