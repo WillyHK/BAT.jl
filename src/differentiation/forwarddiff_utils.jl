965519ba70a076d487d3a30eaf36995aae1009b7
Oliver Schulz <oschulz@mpp.mpg.de>
2021-04-30 19:42:25 +0200

    STASH improve forwarddiff_utils

--- src/differentiation/forwarddiff_utils.jl
+++ src/differentiation/forwarddiff_utils.jl
@@ -7,19 +7,27 @@ function forwarddiff_dualized(::Type{TagType}, x::Vararg{<:Real,N}) where {TagTy
     ntuple(j -> ForwardDiff.Dual{TagType}(x[j], ntuple(i -> i == j, Val(N))), Val(N))
 end
 
-forwarddiff_dualized(::Type{TagType}, x::NTuple{N,T}) where {TagType,N,T<:Real} = forwarddiff_dualized(TagType, x...)
+forwarddiff_dualized(::Type{TagType}, x::Tuple) where {TagType} = forwarddiff_dualized(TagType, x...)
 
 # Equivalent to ForwardDiff internal function dualize(T, x):
-forwarddiff_dualized(::Type{TagType}, x::SVector{N,T}) where {TagType,N,T<:Real} = SVector(forwarddiff_dualized(TagType, x...))
+forwarddiff_dualized(::Type{TagType}, x::SVector) where {TagType} = SVector(forwarddiff_dualized(TagType, x...))
 
 
 
 # Equivalent to ForwardDiff internal function static_dual_eval(TagType, f, x) (for SVector):
-function forwarddiff_eval(f::Base.Callable, x::Union{T, NTuple{N,T}, SVector{N,T}}) where {N,T<:Real}
+#function forwarddiff_eval(f::Base.Callable, x::Union{T, NTuple{N,T}, SVector{N,T}}) where {N,T<:Real}
+#    TagType = typeof(ForwardDiff.Tag(f, T))
+#    x_dual = forwarddiff_dualized(T, x)
+#    y_dual = f(x_dual)
+#end
+
+
+function forwarddiff_eval(f::Base.Callable, x...)
+    T = promote_type(map(eltype, x)...)
     TagType = typeof(ForwardDiff.Tag(f, T))
-    x_dual = forwarddiff_dualized(T, x)
-    y_dual = f(x_dual)
+    f(forwarddiff_dualized(TagType, x...)...)
 end
+
 
 
 forwarddiff_vjp(ΔΩ::Real, y_dual::ForwardDiff.Dual{TagType,T,1}) where {TagType,T<:Real} = ΔΩ * first(ForwardDiff.partials(y_dual))
@@ -61,5 +69,33 @@ function forwarddiff_broadcast_pullback(fs, X::AbstractArray{<:SVector{N,T}}) wh
     Y = broadcast(fs, X)
     fwddiff_back(ΔΩs) = broadcast((f, x, ΔΩ) -> ForwardDiff.jacobian(f, x)' * ΔΩ, fs, X, ΔΩs)
     Y, fwddiff_back
+end
+=#
+
+#=
+
+# Modified version of `dual_function` in Zygote.jl:
+function dual_function(f::F) where F
+    function dualized_f(args::Vararg{Any,N}) where N
+        ds = map(args, ntuple(identity,Val(N))) do x, i
+            dual(x, ntuple(j -> i==j, Val(N)))
+        end
+        return f(ds...)
+    end
+    return dualized_f
+end
+
+
+fwddiff_broadcast(f, args::Vararg{Union{AbstractArray,Number},N}) where N = broadcast(f, args...)
+
+
+@inline function broadcast_forward(f, args::Vararg{Union{AbstractArray,Number},N}) where N
+    T = Broadcast.combine_eltypes(f, args)
+    out = dual_function(f).(args...)
+    eltype(out) <: Dual || return (out, _ -> nothing)
+    y = map(x -> x.value, out)
+    _back(ȳ, i) = unbroadcast(args[i], ((a, b) -> a*b.partials[i]).(ȳ, out))
+    back(ȳ) = ntuple(i -> _back(ȳ, i), N)
+    return y, back
 end
 =#
--- test/differentiation/test_forwarddiff_utils.jl
+++ test/differentiation/test_forwarddiff_utils.jl
@@ -6,14 +6,30 @@ using Test
 using StaticArrays, ForwardDiff
 
 @testset "forwarddiff_utils" begin
+    f = (x...) -> map(x -> norm(x)^2, x)
+    DTf32 = ForwardDiff.Dual{ForwardDiff.Tag{typeof(f),Float32}}
+    @test @inferred(BAT.forwarddiff_eval(f, 3f0)) == (DTf32(9.0,6.0),)
+    DTf64 = ForwardDiff.Dual{ForwardDiff.Tag{typeof(f),Float64}}
+    rdf = (DTf64(4.0,4.0,0.0,0.0), DTf64(9.0,0.0,6.0,0.0), DTf64(16.0,0.0,0.0,8.0))
+    @test @inferred(BAT.forwarddiff_eval(f, 2, 3.0, 4f0)) == rdf
+    @test @inferred(BAT.forwarddiff_eval(f, (2, 3.0, 4f0))) == rdf
+    @test @inferred(BAT.forwarddiff_eval(f, SVector(2, 3.0, 4f0))) == rdf
+
+    g = (x...) -> sum(map(x -> norm(x)^2, x))
+    DTg32 = ForwardDiff.Dual{ForwardDiff.Tag{typeof(g),Float32}}
+    @test @inferred(BAT.forwarddiff_eval(g, 3f0)) == DTg32(9.0,6.0)
+    DTg64 = ForwardDiff.Dual{ForwardDiff.Tag{typeof(g),Float64}}
+    @test @inferred(BAT.forwarddiff_eval(g, 2, 3.0, 4f0)) == DTg64(29.0,4.0,6.0,8.0)
+    @test @inferred(BAT.forwarddiff_eval(g, (2, 3.0, 4f0))) == DTg64(29.0,4.0,6.0,8.0)
+    @test @inferred(BAT.forwarddiff_eval(g, SVector(2, 3.0, 4f0))) == DTg64(29.0,4.0,6.0,8.0)
+
     @test @inferred(ForwardDiff.value(BAT.forwarddiff_eval(sin, 0.5))) == sin(0.5)
     @test @inferred(ForwardDiff.partials(BAT.forwarddiff_eval(sin, 0.5))[1]) == cos(0.5)
     @test length(ForwardDiff.partials(BAT.forwarddiff_eval(sin, 0.5))) == 1
 
     @test @inferred(BAT.forwarddiff_vjp(0.7, BAT.forwarddiff_eval(sin, 0.5))) == 0.7 * ForwardDiff.derivative(sin, 0.5)
 
-
-    f(x) = SVector(1.1 * x[1] + 2.1 * x[2] + 3.1 * x[3], -1.2 * x[1] - 2.2 * x[2] + -3.2 * x[3])
+    f = x -> SVector(1.1 * x[1] + 2.1 * x[2] + 3.1 * x[3], -1.2 * x[1] - 2.2 * x[2] + -3.2 * x[3])
 
     x = SVector(0.1, 0.2, 0.3)
     ΔΩ = SVector((10, 20))
