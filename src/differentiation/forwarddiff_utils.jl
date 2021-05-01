47dcd31196ea984cad3d35ae949795e070fda319
Oliver Schulz <oschulz@mpp.mpg.de>
2021-05-01 16:10:01 +0200

    STASH partieal fwddiff

--- src/differentiation/forwarddiff_utils.jl
+++ src/differentiation/forwarddiff_utils.jl
@@ -1,30 +1,19 @@
 # This file is a part of BAT.jl, licensed under the MIT License (MIT).
 
 
-forwarddiff_dualized(::Type{TagType}, x::Real) where TagType = ForwardDiff.Dual{TagType}(x, one(x))
+function _fu_replace_nth(f::Base.Callable, x::Tuple, ::Val{i}) where i
+    ntuple(j -> i == j ? f(x[j]) : x[j], Val(length(x)))
+end
 
-function forwarddiff_dualized(::Type{TagType}, x::Vararg{Real,N}) where {TagType,N}
+
+forwarddiff_dualized(::Type{TagType}, x::Real) where TagType = ForwardDiff.Dual{TagType}(x, true)
+
+function forwarddiff_dualized(::Type{TagType}, x::NTule{Real,N}) where {TagType,N}
     ntuple(j -> ForwardDiff.Dual{TagType}(x[j], ntuple(i -> i == j, Val(N))), Val(N))
 end
 
-forwarddiff_dualized(::Type{TagType}, x::Tuple) where {TagType} = forwarddiff_dualized(TagType, x...)
-
 # Equivalent to ForwardDiff internal function dualize(T, x):
 forwarddiff_dualized(::Type{TagType}, x::SVector) where {TagType} = SVector(forwarddiff_dualized(TagType, x...))
-
-
-@inline forwarddiff_tagtype(f::Base.Callable, xs...) = typeof(ForwardDiff.Tag(f, promote_type(map(eltype, xs)...)))
-
-
-@inline forwarddiff_eval(f::Base.Callable, xs::Real...) = 
-    f(forwarddiff_dualized(forwarddiff_tagtype(f, xs), xs...)...)
-
-@inline forwarddiff_eval(f::Base.Callable, x::NTuple{N,Real}) where N =
-    f(forwarddiff_dualized(forwarddiff_tagtype(f, x), x))
-
-# Equivalent to ForwardDiff internal function static_dual_eval(TagType, f, x) (for SVector):
-@inline forwarddiff_eval(f::Base.Callable, x::SVector{N,<:Real}) where N =
-    f(forwarddiff_dualized(forwarddiff_tagtype(f, x), x))
 
 
 @inline forwarddiff_value(y_dual::Real) = ForwardDiff.value(y_dual)
@@ -32,7 +21,7 @@ forwarddiff_dualized(::Type{TagType}, x::SVector) where {TagType} = SVector(forw
 @inline forwarddiff_value(y_dual::SVector{N,<:Real}) where N = SVector(map(ForwardDiff.value, y_dual))
 
 
-@inline forwarddiff_vjp(ΔΩ::Real, y_dual::Real) = (ΔΩ * ForwardDiff.partials(y_dual)...,)
+@inline forwarddiff_vjp(ΔΩ::Real, y_dual::Real) = ΔΩ * ForwardDiff.partials(y_dual)
 
 function forwarddiff_vjp(ΔΩ::NTuple{N,Real}, y_dual::NTuple{N,Real}) where N
     (sum(map((ΔΩ_i, y_dual_i) -> ForwardDiff.partials(y_dual_i) * ΔΩ_i, ΔΩ, y_dual))...,)
@@ -47,9 +36,23 @@ end
     forwarddiff_vjp((ΔΩ...,), (y_dual...,))
 end
 
-@inline forwarddiff_vjp(::Type{<:Real}, ΔΩ, y_dual) = forwarddiff_vjp(ΔΩ, y_dual)
-@inline forwarddiff_vjp(::Type{<:Tuple}, ΔΩ, y_dual) = (forwarddiff_vjp(ΔΩ, y_dual),)
-@inline forwarddiff_vjp(::Type{<:SVector}, ΔΩ, y_dual) = (SVector(forwarddiff_vjp(ΔΩ, y_dual)),)
+@inline forwarddiff_vjp(::Type{<:Real}, ΔΩ, y_dual) = forwarddiff_vjp(ΔΩ, y_dual)::Real
+@inline forwarddiff_vjp(::Type{<:Tuple}, ΔΩ, y_dual) = forwarddiff_vjp(ΔΩ, y_dual)::Tuple
+@inline forwarddiff_vjp(::Type{<:SVector}, ΔΩ, y_dual) = SVector(forwarddiff_vjp(ΔΩ, y_dual)::Tuple)
+
+
+@inline function partial_forwarddiff_eval(f::Base.Callable, xs::Tuple, ::Val{i}) where i
+    TagType = typeof(ForwardDiff.Tag((f,Val(i)), eltype(args[i])))
+    dualized_args = _fu_replace_nth(x_i -> forwarddiff_dualized(TagType, x_i), xs, Val(i))
+    f(dualized_args...)
+end
+
+@inline function partial_forwarddiff_fwd_back(f::Base.Callable, xs::Tuple, ::Val{i}, ΔΩ) where i
+    @info "RUN partial_forwarddiff_fwd_back(f, xs, $i, ΔΩ)"
+    x_i = xs[i]
+    y_dual = partial_forwarddiff_eval(f, xs, Val(i))
+    forwarddiff_vjp(typeof(x_i), ΔΩ, y_dual)
+end
 
 
 struct FwdDiffDualBack{TX,TY} <: Function
@@ -57,12 +60,13 @@ struct FwdDiffDualBack{TX,TY} <: Function
 end
 
 function (back::FwdDiffDualBack{TX})(ΔΩ) where TX
-    # @info "RUN BACK" typeof(ΔΩ)
+    # @info "RUN BACK (back::FwdDiffDualBack{TX})(ΔΩ)"
     (ChainRulesCore.NO_FIELDS, forwarddiff_vjp(TX, ΔΩ, back.y_dual)...)
 end
 
-function forwarddiff_pullback(f::Base.Callable, xs...)
-    # @info "RUN forwarddiff_pullback"
+
+function forwarddiff_pullback(f::Base.Callable, xs::Vararg{N,Any}) where N
+
     y_dual = forwarddiff_eval(f, xs...)
     y = forwarddiff_value(y_dual)
     y, FwdDiffDualBack{eltype(xs), typeof(y_dual)}(y_dual)
@@ -75,15 +79,71 @@ end
 
 @inline (wrapped_f::WithForwardDiff)(xs...) = wrapped_f.f(xs...)
 
-@inline ChainRulesCore.rrule(wrapped_f::WithForwardDiff{F}, xs...) where F = forwarddiff_pullback(wrapped_f.f, xs...)
+@inline function ChainRulesCore.rrule(wrapped_f::WithForwardDiff, xs::Vararg{N,Any}) where N
+    @info "RUN ChainRulesCore.rrule(wrapped_f::WithForwardDiff, xs)"
+    f = wrapped_f.f
+    y = f(xs...)
+    function with_fwddiff_pullback(ΔΩ)
+        return (
+            NO_FIELDS,
+            ntuple(i -> @thunk(partial_forwarddiff_fwd_back(f, xs, Val(i), ΔΩ)), Val(N))...
+        )
+    end
+end
 
 
-
-struct DualizedFunction{F} <: Function
+#=
+struct DualizedFunction{F<:Base.Callable,i} <: Function
     f::F
 end
 
-@inline (dualized_f::DualizedFunction)(x...) = forwarddiff_eval(dualized_f.f, x...)
+DualizedFunction(f::F, ::Val{i}) where {F<:Base.Callable,i} = DualizedFunction(F,i)(f)
+
+@inline (dualized_f::DualizedFunction{F,i})(x...) where {F,i} = partial_forwarddiff_eval(dualized_f.f, xs, Val(i))
+=#
 
 
 # ToDo: ChainRulesCore.rrule(::typeof(Base.broadcasted), wrapped_f::WithForwardDiff, xs...)
+
+
+#=
+
+function rrule(
+    ::typeof(*),
+    A::AbstractVecOrMat{<:CommutativeMulNumber},
+    B::AbstractVecOrMat{<:CommutativeMulNumber},
+)
+    function times_pullback(Ȳ)
+        return (
+            NO_FIELDS,
+            InplaceableThunk(
+                @thunk(Ȳ * B'),
+                X̄ -> mul!(X̄, Ȳ, B', true, true)
+            ),
+            InplaceableThunk(
+                @thunk(A' * Ȳ),
+                X̄ -> mul!(X̄, A', Ȳ, true, true)
+            )
+        )
+    end
+    return A * B, times_pullback
+end
+
+
+function rrule(
+    ::typeof(*), A::CommutativeMulNumber, B::AbstractArray{<:CommutativeMulNumber}
+ )
+     function times_pullback(Ȳ)
+         return (
+             NO_FIELDS,
+             @thunk(dot(Ȳ, B)'),
+             InplaceableThunk(
+                 @thunk(A' * Ȳ),
+                 X̄ -> mul!(X̄, conj(A), Ȳ, true, true)
+             )
+         )
+     end
+     return A * B, times_pullback
+ end
+ 
+=#
